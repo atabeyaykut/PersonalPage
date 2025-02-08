cb5a27e37b38a1f63b17b8fb3e1daea9d415be4d
kantlori <atabeywade@gmail.com>
2025-02-09 01:19:39 +0300

    Enhance SEO and accessibility by adding meta tags in index.html; restructure HomePage component for improved semantics and navigation

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,26 +1,34 @@
 import React from 'react'
 import { FaGithub } from "react-icons/fa";
-import { TbBrandGithubFilled } from "react-icons/tb";
 import { FaLinkedinIn } from "react-icons/fa6";
 
 function HomePage() {
     return (
-        <section className="home page-transition">
-            <div className="home-content">
+        <main className="home page-transition">
+            <article className="home-content">
                 <h1>Hi, I'm Atabey AYKUT</h1>
-                <h3>Frontend Developer</h3>
+                <h2 className="h3">Frontend Developer</h2>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae est, quisquam, nobis quos fuga nemo sint voluptate vel, voluptas quam voluptates. Quaerat, nostrum! Eos asperiores sunt atque.</p>
-                <div className="btn-box">
-                    <a href="#">Hire Me</a>
-                    <a href="#">Lets Talk</a>
-                </div>
-            </div>
-            <div className="home-sci">
-                <a href="#"><FaGithub /></a>
-                <a href="#"><TbBrandGithubFilled /></a>
-                <a href="#"><FaLinkedinIn /></a>
-            </div>
-        </section>
+                <nav className="btn-box">
+                    <a href="#contact" aria-label="Hire Me">Hire Me</a>
+                    <a href="#contact" aria-label="Let's Talk">Lets Talk</a>
+                </nav>
+            </article>
+            <nav className="home-sci">
+                <a href="https://github.com/atabeyaykut/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    aria-label="GitHub Profile">
+                    <FaGithub />
+                </a>
+                <a href="https://www.linkedin.com/in/atabeyaykut/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    aria-label="LinkedIn Profile">
+                    <FaLinkedinIn />
+                </a>
+            </nav>
+        </main>
     )
 }
 
